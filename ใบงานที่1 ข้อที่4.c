731ec7627a6bf67cfdc92dfc496829d1b2d3c265
Peeranut053 <tapeeranut09936@gmail.com>
2023-07-17 20:05:19 +0700

    Create ใบงานที่1 ข้อที่4.c

---
+++ ใบงานที่1 ข้อที่4.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    
+    
+    const float pi=3.14;
+    float r,area;
+     
+    printf("Enter your radian : ");
+    scanf("%f",&r);
+    
+    printf("%.2f",pi*(r*r));
+    
+    
+
+    return 0;
+}
+
+
+
