da3784fd1344ea2400b1c0bc1cef59b3256b2690
Peeranut053 <tapeeranut09936@gmail.com>
2023-07-17 20:02:16 +0700

    Create ใบงานที่1 ข้อที่3.c

---
+++ ใบงานที่1 ข้อที่3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    
+    float n1, n2, n3,result; 
+    printf("Number 1 : ");
+    scanf("%f",&n1);
+    printf("Number 2 : ");
+    scanf("%f",&n2);
+    printf("Number 3 : ");
+    scanf("%f",&n3);
+    printf("Results = %.3f",(n1+n2+n3)/3);
+    
+    return 0;
+}
+
+
+
