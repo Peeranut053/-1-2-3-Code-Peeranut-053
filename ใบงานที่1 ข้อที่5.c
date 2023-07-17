5db1ba42a06a2480e8031de9d977192e601166b1
Peeranut053 <tapeeranut09936@gmail.com>
2023-07-17 20:08:01 +0700

    Create ใบงานที่1 ข้อที่5.c

---
+++ ใบงานที่1 ข้อที่5.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int n1;
+    printf("Enter numeber : ");
+    scanf("%d",&n1);
+    n1++;
+    printf("%d\n",n1);
+    n1++;
+    printf("%d\n",n1);
+    n1++;
+    printf("%d",n1);
+    
+    
+    
+    
+
+    return 0;
+}
+
+
+
