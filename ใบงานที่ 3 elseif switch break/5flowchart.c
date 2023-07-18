67652a0af6bf4ded01c68f83d7add362e00c512e
Peeranut053 <tapeeranut09936@gmail.com>
2023-07-18 10:52:00 +0700

    053

--- ใบงานที่ 3 elseif switch break/5flowchart.c
+++ ใบงานที่ 3 elseif switch break/5flowchart.c
@@ -8,48 +8,57 @@ Write your code in this editor and press "Run" button to compile and execute it.
 
 #include <stdio.h>
 
-int main()
+int
+main ()
 {
-    int a,b,c,d,swap;
-    printf("Input A : ");
-    scanf("%d",&a);
-    printf("Input B : ");
-    scanf("%d",&b);
-    printf("Input C : ");
-    scanf("%d",&c);
-    printf("Input D : ");
-    scanf("%d",&d);
-    
-    if(a>b){
-        swap=a;
-        a=b;
-        b=swap;
-        if(d>=a){
-            if(c>d){
-                c-=a;
-            }
-            else
-            c+=a;
-        }
-        b=a+b+c;
-    }
-    else
+  int a, b, c, d, swap;
+  printf ("Input A : ");
+  scanf ("%d", &a);
+  printf ("Input B : ");
+  scanf ("%d", &b);
+  printf ("Input C : ");
+  scanf ("%d", &c);
+  printf ("Input D : ");
+  scanf ("%d", &d);
+
+  if (a > b)
     {
-        if(c>a>=b){
-            d+=a;
-        }
-        else{
-            if(d>c){
-                b+=2;
-            }
-            else{
-                b=2*b;
-            }
-        }
+      swap = a;
+      a = b;
+      b = swap;
+      if (d >= a)
+	{
+	  if (c > d)
+	    {
+	      c = c - a;
+	    }
+
+	}
+      else
+	{
+	  c = c + a;
+	}
+	b=a+c+d;
     }
-    printf("A = %d\nB = %d\nC = %d\nD = %d",a,b,c,d);
-    
-    return 0;
+else
+{
+  if (c > a && a >= b)
+    {
+      d += a;
+    }
+  {
+    if (d > c)
+      {
+	b += 2;
+      }
+    else
+      {
+	b = 2 * b;
+      }
+  }
 }
 
+printf ("A = %d\nB = %d\nC = %d\nD = %d", a, b, c, d);
 
+return 0;
+}
