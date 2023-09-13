1106d39a3f5eadc6ae48687d4cdee50c3cce0e0e
Peeranut053 <tapeeranut09936@gmail.com>
2023-09-13 15:56:37 +0700

    Create array.c

---
+++ ใบงาน Array/array.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[] = {5, 4, 3, 2, 1};
+    
+    for(int i = 0; i < 5; i++){
+        printf("Arr[%d] : %d\n",i,arr[i]);
+    }
+    
+    return 0;
+}
